173c92b3fc393f33a1b33653c45fe5d90e136dc9
BhargavaK2012 <106030283+BhargavaK2012@users.noreply.github.com>
2023-12-03 17:35:40 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -5,10 +5,33 @@ public class Main {
     public int healthy = 0;
     public int exchange = 0;
     public int failed = 0;
+    public int rated_capacity = 120; // initial capacity mentioned in question
+    public int[] sohPercentage; // used to store the state of health of each battery
+
   };
+
+   public int[] countsBySoh(int[] presentCapacities) {
+            sohPercentage = new int[presentCapacities.length];
+            for (int capacity : presentCapacities) {
+                // calculating the soh
+                int percentage = (100 * capacity) / rated_capacity;
+                sohPercentage[healthy + failed + exchange] = percentage;
+                // classification and counting according to each category
+                if (percentage > 80) {
+                    healthy++;
+                } else if (percentage <= 80 && percentage >= 62) {
+                    exchange++;
+                } else {
+                    failed++;
+                }
+            }
+            return sohPercentage;
+        }
+    };
 
   static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
+    counts.countsBySoh(presentCapacities);
     return counts;
   }
 
